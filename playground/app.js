0cc8de2cd2bdec0ad25b99685416326bf7f87403
Raylex Lee <raylexym57lee@gmail.com>
2019-04-10 00:53:09 +0800

    Update app.js

--- playground/app.js
+++ playground/app.js
@@ -1,9 +1,10 @@
 const proxyurl = "https://peaceful-earth-25999.herokuapp.com/";
-const url = "http://podcast.rthk.hk/podcast/item_epi.php?pid=287"; // site that doesn’t send Access-Control-*
-const re = new XRegExp('<body(.*)<\\/body>','s')
+const url = "http://podcast.rthk.hk/podcast/item_all.php?pid=287&lang=zh-CN"; // site that doesn’t send Access-Control-*
+const reBody = new XRegExp('^.*<body[^>]*>(.*)<\\/body>.*$','s')
+const reScript = new XRegExp('<script[^>]*>.*?<\\/script>','gs')
 fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/http://esda.wyk.edu.hk/status/staff.html
 .then(response => response.text())
 .then(function(contents) {  
-    const responseText = contents.replace(re, '$1');
+    const responseText = contents.replace(reBody, '$1').replace(reScript, '');
     console.log(responseText)})
 .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
