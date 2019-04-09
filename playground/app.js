4d5add7373fdf968c03263111d0ca3be45539f1c
Raylex Lee <raylexym57lee@gmail.com>
2019-04-10 01:01:56 +0800

    Update app.js

--- playground/app.js
+++ playground/app.js
@@ -2,7 +2,7 @@ const proxyurl = "https://peaceful-earth-25999.herokuapp.com/";
 const url = "http://podcast.rthk.hk/podcast/item_all.php?pid=287&lang=zh-CN"; // site that doesn’t send Access-Control-*
 const reBody = new XRegExp('^.*<body[^>]*>(.*)<\\/body>.*$','s')
 const reScript = new XRegExp('<script[^>]*>.*?<\\/script>','gs')
-fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/http://esda.wyk.edu.hk/status/staff.html
+fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/http://podcast.rthk.hk/podcast/item_all.php?pid=287&lang=zh-CN
 .then(response => response.text())
 .then(function(contents) {  
     const responseText = contents.replace(reBody, '$1').replace(reScript, '');
