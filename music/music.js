7eb4d781ade4fd039af8f05eb373972a704a5095
raylexlee <raylexym57lee@gmail.com>
2020-10-31 20:18:18 +0800

    Add gotoSong and shscript

--- music/music.js
+++ music/music.js
@@ -1,25 +1,45 @@
+var Album, videoIds, arg;
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
 const IframeHTML = videoId => `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`; 
-function outputHTML() {
-    const i = getRandomIntInclusive(0, videoIds.length - 1);
-    document.getElementById('myAlbum').value = videoIds[i];
-    document.getElementById('musicvideo').innerHTML = IframeHTML(videoIds[i]);
+const optSong = timeTitle => {
+  const start = ('Start' in timeTitle) ? timeTitle.start : '';
+  const song = ('Artist' in timeTitle ) ? `${timeTitle.Artist}-${timeTitle.Title}` : timeTitle.Title;
+  return `<option value="${start}">${song.replace(/\\/g, '')}`;
+};
+function outputHTML(arg) {
+    const link = arg.link;
+    const album = arg.albumObj;
+    const GetShScriptFunc = /\(\?<t>/.test(album.regex) ? xgetsongs : getsongs;
+    const r = GetShScriptFunc(arg);
+    document.getElementById("bashscript").value = r.shscript;
+    document.getElementById("mySong").innerHTML = r.TimeTitles.map(id => optSong(id)).join('\n')
+    document.getElementById('myAlbum').value = link;
+    document.getElementById('musicvideo').innerHTML = IframeHTML(link);
 } 
 function gotoAlbum() {
-  const videoId = document.getElementById("myAlbum").value;
-  document.getElementById('musicvideo').innerHTML = IframeHTML(videoId);
+    const videoId = document.getElementById("myAlbum").value;
+    outputHTML({link: videoId, albumObj: Album[videoId]})
+}
+function gotoSong() {
+    const start = document.getElementById("mySong").value;
+    if (start) {
+        const videoId = document.getElementById("myAlbum").value;
+        const link = `${videoId}?Start=${start}`;
+        document.getElementById('musicvideo').innerHTML = IframeHTML(link);
+    }
 }
 const optAlbum = videoId => `<option value="${videoId}">${Album[videoId].album}`;
-var Album, videoIds;
 fetch('album.json')
     .then(response => response.json())
     .then(data => { 
       Album = data;
       videoIds = Object.keys(Album);
       document.getElementById("myAlbum").innerHTML = videoIds.map(id => optAlbum(id)).join('\n')
-      outputHTML();
+      const i = getRandomIntInclusive(0, videoIds.length - 1);
+      arg = {link: videoIds[i], albumObj: Album[videoIds[i]]};
+      outputHTML(arg);
     });
---
+++ music/xgetSongs.js
@@ -0,0 +1,43 @@
+const xgetsongs = arg => {
+    const link = `https://www.youtube.com/watch?v=${arg.link}`;
+    const album = arg.albumObj;
+    const bashEscapePattern = /[\]\['&!?,\s()]/g;
+    const escapePattern = bashEscapePattern;
+    const pattern = XRegExp(album.regex, 'x');
+    const hasTime = /\(\?<m>/.test(album.regex);
+    const hasHour = /\(\?<h>/.test(album.regex);
+    const hasArtist = /\(\?<a>/.test(album.regex);
+    const hasYear = /\(\?<y>/.test(album.regex);
+    const TimeTitles = album.timetitles.map(timetitle => {
+        const r = XRegExp.exec(timetitle, pattern);
+        const hour = hasHour ? r.h.replace(/:$/,'') : '0';
+        const Time = hasTime ? `${60*hour + 1*r.m}.${r.s}` : '';
+        const Start = hasTime ? (3600*hour + 60*r.m + 1*r.s) : 0;
+        const Title = r.t.replace(escapePattern, '\\$&');
+        const Artist = hasArtist ? r.a.replace(escapePattern, '\\$&') : '';
+        const Year = hasYear ? r.y : '';
+        return hasTime 
+            ? {Time: Time, Title: Title, Artist: Artist, Year: Year}
+            : {Start: Start, Time: Time, Title: Title, Artist: Artist, Year: Year};
+    });
+    const Times = TimeTitles.map(e => e.Time);
+    const artist = album.singer.replace(escapePattern, '\\$&');
+    const alBum = album.album.replace(escapePattern, '\\$&');
+    const Titles = TimeTitles.map( (e, idx) => { 
+        let TitleStr = (idx === 0 )  ? `@b=${alBum},@t=${e.Title},@g=13` : `@t=${e.Title}`;
+        TitleStr = hasYear ? `${TitleStr},@y=${e.Year}` : TitleStr;
+        TitleStr = hasArtist ? `${TitleStr},@a=${e.Artist}` 
+                             : ((idx === 0) ? `${TitleStr},@a=${artist}` : TitleStr);
+        return (idx === 0) ? `%[${TitleStr}]` : `[${TitleStr}]`;                     
+    }); 
+    const TimeArg = `${Times.join(" ")} EOF`;
+    const TitleArg = Titles.join('');
+    const mkdirLine = `mkdir ~/NewMusic/${artist}`;
+    const dlmp3Line = `youtube-dl --extract-audio --audio-format mp3 --audio-quality 5 -o 'album.%(ext)s' ${link}`;
+    const albumFile = 'album.mp3';
+    const spltLine = hasTime 
+      ? `mp3splt -o @a-@t -d ~/NewMusic/${artist} -g ${TitleArg} ${albumFile} ${TimeArg}`
+      : `mp3splt -s -o @a-@t -d ~/NewMusic/${artist} -g ${TitleArg} ${albumFile}`;
+    return {shscript: `${mkdirLine}\n${dlmp3Line}\n${spltLine}\nrm album.mp3\n`,
+            TimeTitles: TimeTitles} ;  
+}
