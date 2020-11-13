382c97f28f919c2cc85391ff6338f0c26589d855
raylexlee <raylexym57lee@gmail.com>
2020-11-13 18:56:16 +0800

    Fine tune the parameters.

--- wypath_campus_tour/tour.js
+++ wypath_campus_tour/tour.js
@@ -1,4 +1,4 @@
-const panoIframe = loc => `<iframe width="800" height="400" allowfullscreen style="border-style:none;"
+const panoIframe = loc => `<iframe width="800" height="500" allowfullscreen style="border-style:none;"
 src="https://cdn.pannellum.org/2.5/pannellum.htm#panorama=https%3A//raylexlee.github.io/wypath_campus_tour/dest_pano/${loc}.jpg&autoLoad=true"></iframe>`;
 const maparea = imageMapResize();
 maparea.forEach(a => a.onclick=function(item,index) {
@@ -7,3 +7,4 @@ maparea.forEach(a => a.onclick=function(item,index) {
 //    const alt = ('path' in item) ? item.path[0].alt : item.target.alt;
     document.getElementById('pano').innerHTML = panoIframe(item.target.alt);
 });
+document.getElementById('pano').innerHTML = panoIframe("1");
