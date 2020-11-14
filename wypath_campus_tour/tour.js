b573ae5677a8a756140ba02a2399441ab5cf59ec
raylexlee <raylexym57lee@gmail.com>
2020-11-14 13:08:21 +0800

    Beautify the codes.

--- wypath_campus_tour/tour.js
+++ wypath_campus_tour/tour.js
@@ -1,10 +1,8 @@
 const panoIframe = loc => `<iframe width="800" height="500" allowfullscreen style="border-style:none;"
-src="https://cdn.pannellum.org/2.5/pannellum.htm#panorama=https%3A//raylexlee.github.io/wypath_campus_tour/dest_pano/${loc}.jpg&autoLoad=true"></iframe>`;
-const maparea = imageMapResize();
-maparea.forEach(a => a.onclick=function(item,index) {
-    item.preventDefault();
-    console.log(item);
-//    const alt = ('path' in item) ? item.path[0].alt : item.target.alt;
-    document.getElementById('pano').innerHTML = panoIframe(item.target.alt);
-});
+    src="https://cdn.pannellum.org/2.5/pannellum.htm#panorama=https%3A//raylexlee.github.io/wypath_campus_tour/dest_pano/${loc}.jpg&autoLoad=true"></iframe>`;
+const maps = imageMapResize();
+maps[0].onclick = function (MapClick) {
+    MapClick.preventDefault();
+    document.getElementById('pano').innerHTML = panoIframe(MapClick.target.alt);
+};
 document.getElementById('pano').innerHTML = panoIframe("1");
