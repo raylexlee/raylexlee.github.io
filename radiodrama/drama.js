d79a332aaf3434ef4875e49a0323e83ba039455f
raylexlee <raylexym57lee@gmail.com>
2020-11-23 00:32:01 +0800

    Update album and playlist jsons

---
+++ radiodrama/drama.js
@@ -0,0 +1,8 @@
+var Album, videoIds, arg;
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+const IframeHTML = playlistId => `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/videoseries?list=${playlistId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`; 
+
