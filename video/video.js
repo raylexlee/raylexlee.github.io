b8c419215b6ce24c68cee93c92222b376cd813d0
raylexlee <raylexym57lee@gmail.com>
2020-12-01 01:38:43 +0800

    Shorten winWidth from 0.6 to 0.55 scale

--- video/video.js
+++ video/video.js
@@ -27,7 +27,7 @@ function getTextWidth(text, font) {
     return metrics.width;
 }
 function GetTextLengths() {
-  const winWidth = window.outerWidth * 0.6;
+  const winWidth = window.outerWidth * 0.55;
   const myFont = document.getElementById("myPlaylist").style.font;
   const englishWidth = getTextWidth(EnglishName, myFont) / 10;
   const chineseWidth = getTextWidth(ChineseName, myFont) / 5;
