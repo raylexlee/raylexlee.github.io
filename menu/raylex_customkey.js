6190743d0777c1b7e67826e0d3dc73dc1ac43d80
raylexlee <raylexym57lee@gmail.com>
2020-12-14 12:54:33 +0800

    Streamline codes.

--- menu/raylex_customkey.js
+++ menu/raylex_customkey.js
@@ -35,24 +35,30 @@ function copyHintToClipboard() {
 }
 
 function playSongsRandom() {
-  if (Songs.length === 0) {
-    const b = document.getElementsByTagName('a');
-    let m = null;
-    let i = -1;
-    while (m === null) {
-      i++;
-      m = b[i].href.match(/\('(.*)'\)/);
-    }
-    while (m !== null) {
-      Songs.push(m[1]);
-      i++;
-      m = b[i].href.match(/\('(.*)'\)/);
-    }
+  if (Songs.length === 0) fillUpSongs();
+  player.loadPlaylist(getRandomsWithin(Songs.length, 20).map(i => Songs[i]), 0, 0);
+}
+
+function fillUpSongs() {
+  const b = document.getElementsByTagName('a');
+  let m = null;
+  let i = -1;
+  while (m === null) {
+    i++;
+    m = b[i].href.match(/\('(.*)'\)/);
   }
+  while (m !== null) {
+    Songs.push(m[1]);
+    i++;
+    m = b[i].href.match(/\('(.*)'\)/);
+  }
+}
+
+function getRandomsWithin(Length, Number) {
   const arr = [];
-  while (arr.length < 20) {
-    const r = Math.floor(Math.random() * Songs.length);
+  while (arr.length < Number) {
+    const r = Math.floor(Math.random() * Length);
     if (arr.indexOf(r) === -1) arr.push(r);
   }
-  player.loadPlaylist(arr.map(i => Songs[i]), 0, 0);
+  return arr;
 }
