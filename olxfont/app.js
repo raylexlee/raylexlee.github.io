3a23d1d786879f16a11a82481452dd389c4a22b4
raylexlee <raylexym57lee@gmail.com>
2021-02-04 22:36:38 +0800

    Redesign by using json.

--- olxfont/app.js
+++ olxfont/app.js
@@ -1,6 +1,14 @@
+let x;
 const imgSrc = c => `http://open-lit.com/mpf/${c.substr(0,1).toLowerCase()}/${c.toUpperCase()}.BMP`;
 const imgElement = c => 
   `<img src="${imgSrc(c)}" align=absmiddle border=0>`;
+fetch('xtext.json')
+    .then(response => response.json())
+    .then(data => { 
+      x = data;
+      LayoutTable();
+    });
+function LayoutTable() {      
 document.querySelector('div').innerHTML = ` <table> <tr>
 ${ Object.keys(x)
   .map(key => `<td>${key}</td>
@@ -9,12 +17,10 @@ ${ Object.keys(x)
   <td>${x[key]}</td>`)
   .join('</tr><tr>') }
         </tr></table>`;
+}        
 function CopyCodes() {
   const textHint = document.getElementById('idHint');
-  textHint.value = Object.keys(x).filter(e => document.getElementsByName(e)[0].checked)
-    .map(e => 
-      `.replace(/<[^<]+\\/${e}\\.BMP[^>]+>/gm, '${x[e]}')`)
-    .join('\n');
+  textHint.value = JSON.stringify(x);
   const r = document.createRange();
   r.selectNode(textHint);
   window.getSelection().removeAllRanges();
