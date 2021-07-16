65bce86de06204313f9cf40acf1ac63a2444d219
raylexlee <raylexym57lee@gmail.com>
2021-07-16 20:56:48 +0800

    Merge ytiframe

---
+++ ytalbum/ytiframeplayer.js
@@ -0,0 +1,153 @@
+let player;
+let Songs = [];
+const howtoId = ['cg7OJWTGnaY', 'cg7OJWTGnaY']
+
+const lsTime = ytId => `${ytId}Time`;
+const lsIndex = ytId => `${ytId}Index`;
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+const IframeHTML = (playlistId, isList = false) =>
+  '<iframe id="idIframe" width="560" height="315" src="https://www.youtube-nocookie.com/embed/' +
+  (isList ?
+    ('playlistseries?enablejsapi=1&list=' + playlistId) :
+    (playlistId + '?enablejsapi=1')) +
+  '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+function genIframeHTML(playlistId, isList) {
+  document.querySelector(".videoWrapper").innerHTML = IframeHTML(playlistId, isList);
+}
+function _outputHTML(playlistId, isList) {
+  if (isList) {
+    if (!localStorage.getItem(lsTime(playlistId))) {
+      localStorage.setItem(lsTime(playlistId), 0.0);
+      localStorage.setItem(lsIndex(playlistId), "0");
+      }
+    const Time = localStorage.getItem(lsTime(playlistId));
+    const Index = localStorage.getItem(lsIndex(playlistId));
+    player.loadPlaylist({list: playlistId,
+                     listType: 'playlist',
+                     index: Index,
+                     startSeconds: Time});
+    player.setLoop(true);                 
+  }  else {
+    if (!localStorage.getItem(lsTime(playlistId))) {
+      localStorage.setItem(lsTime(playlistId), 0.0);
+      }
+    const Time = localStorage.getItem(lsTime(playlistId));
+    player.loadVideoById({videoId: playlistId,
+                      startSeconds: Time});  
+  }
+}
+function outputHTML(playlistId, isList) {
+  SaveCurrentPlayer();
+  _outputHTML(playlistId, isList);
+}
+function PlayYT() {
+  const pasteText = document.getElementById('videoId');
+  const url = pasteText.value;
+  let isAlist=false;
+  const m = url.match(/^https:\/\/youtube\.com\/playlist\?list=(.*)$/);
+  let videoId = '';
+  if (m !== null) {
+    videoId = m[1];
+    isAlist = true; 
+  } else {
+    const v = url.match(/^https:\/\/youtu\.be\/(.*)$/);
+    if (v !== null) videoId = v[1];    
+  }
+  if (videoId === "") videoId = howtoId[getRandomIntInclusive(0,1)];
+  genIframeHTML(videoId, isAlist);
+}
+genIframeHTML(howtoId[getRandomIntInclusive(0,1)]);
+const tag = document.createElement('script');
+tag.id = 'iframe-demo';
+tag.src = 'https://www.youtube.com/iframe_api';
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function SaveCurrentPlayer(startOver = false) {
+  const url = player.getVideoUrl();
+  const m = url.match(/list=([^&]+)&/);
+  if (m !== null) {
+    localStorage.setItem(lsTime(m[1]), player.getCurrentTime());
+    localStorage.setItem(lsIndex(m[1]), player.getPlaylistIndex());
+    if (startOver) {
+      localStorage.setItem(lsTime(m[1]), 0);
+      localStorage.setItem(lsIndex(m[1]), 0);
+      // player.playVideoAt(0);
+    }
+    return;
+    }
+  const v = url.match(/v=([^=]+)$/);
+  if (v !== null) {
+    localStorage.setItem(lsTime(v[1]), player.getCurrentTime());
+    if (startOver) {
+      localStorage.setItem(lsTime(v[1]), 0);
+      //player.playVideo();
+      if (player.getPlaylist() === null) player.loadVideoById(v[1], 0);
+    }
+    return;
+    } 
+}
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('idIframe', {
+    events: {
+      'onReady': onPlayerReady,
+      'onError': onPlayerError,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  // _outputHTML(mOpeningVideoId[1]);
+}
+
+function onPlayerStateChange(event) {
+  switch (event.data) {
+    case 0:
+    case 1:
+    case 2:
+      SaveCurrentPlayer(event.data === 0);
+      break;
+  }
+}
+
+function onPlayerError(event) {
+  console.log(player.getVideoUrl());
+}
+
+function playSongsRandom() {
+  if (Songs.length === 0) fillUpSongs();
+  player.loadPlaylist(getRandomsWithin(Songs.length, 20).map(i => Songs[i]), 0, 0);
+  player.setLoop(true);                 
+}
+
+function fillUpSongs() {
+  const b = document.getElementsByTagName('a');
+  let m = null;
+  let i = -1;
+  while (m === null) {
+    i++;
+    m = b[i].href.match(/\('(.*)'\)/);
+  }
+  while (m !== null) {
+    Songs.push(m[1]);
+    i++;
+    m = b[i].href.match(/\('(.*)'\)/);
+  }
+}
+
+function getRandomsWithin(Length, Number) {
+  const arr = [];
+  while (arr.length < Number) {
+    const r = Math.floor(Math.random() * Length);
+    if (arr.indexOf(r) === -1) arr.push(r);
+  }
+  return arr;
+}
