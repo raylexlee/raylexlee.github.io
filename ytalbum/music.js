024f676192d567af3e8f9721e343413ea521650a
raylexlee <raylexym57lee@gmail.com>
2021-07-16 23:03:51 +0800

    Add album & artist

--- ytalbum/music.js
+++ ytalbum/music.js
@@ -1,10 +1,4 @@
-var Album, videoIds, arg;
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
-}
-const IframeHTML = videoId => `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`; 
+const musicIframeHTML = videoId => `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`; 
 const optSong = timeTitle => {
   const start = ('Start' in timeTitle) ? timeTitle.Start : '';
   const song = ('Artist' in timeTitle ) ? `${timeTitle.Artist}-${timeTitle.Title}` : timeTitle.Title;
@@ -12,26 +6,33 @@ const optSong = timeTitle => {
       ? `<option value="${start}">${timestamp(timeTitle.Time)} ${song.replace(/\\/g, '')}`
       : `<option value="">${song.replace(/\\/g, '')}`;
 };
-function outputHTML(arg) {
-    const link = arg.link;
-    const album = arg.albumObj;
-    const GetShScriptFunc = /\(\?<t>/.test(album.regex) ? xgetsongs : getsongs;
+function CompileRegex() {
+    const pasteText = document.getElementById('videoId');
+    const url = pasteText.value;
+    let videoId = '';
+    const v = url.match(/^https:\/\/youtu\.be\/(.*)$/);
+    if (v !== null) videoId = v[1];
+    if (videoId === '') return;    
+    const regex = document.getElementById('myRegex').value;
+    if (regex === '') return;
+    const GetShScriptFunc = /\(\?<t>/.test(regex) ? xgetsongs : getsongs;
     const r = GetShScriptFunc(arg);
     document.getElementById("mySong").innerHTML = r.TimeTitles.map(id => optSong(id)).join('\n')
     document.getElementById('myAlbum').value = link;
     document.getElementById('musicvideo').innerHTML = IframeHTML(link);
     document.getElementById('myScript').value = r.shscript;
 } 
-function gotoAlbum() {
-    const videoId = document.getElementById("myAlbum").value;
-    outputHTML({link: videoId, albumObj: Album[videoId]})
-}
 function gotoSong() {
     const start = document.getElementById("mySong").value;
     if (start) {
-        const videoId = document.getElementById("myAlbum").value;
+        const pasteText = document.getElementById('videoId');
+        const url = pasteText.value;
+        let videoId = '';
+        const v = url.match(/^https:\/\/youtu\.be\/(.*)$/);
+        if (v !== null) videoId = v[1];
+        if (videoId === '') return;    
         const link = `${videoId}?start=${start}&autoplay=1`;
-        document.getElementById('musicvideo').innerHTML = IframeHTML(link);
+        document.querySelector(".videoWrapper").innerHTML = musicIframeHTML(link);
     }
 }
 const timestamp = splitTime => {
@@ -41,14 +42,3 @@ const timestamp = splitTime => {
   const h = (d===0) ? '' : `${d}:`;
   return `${h}${r}:${s[1]}`; 
 };
-const optAlbum = videoId => `<option value="${videoId}">${Album[videoId].album}`;
-fetch('album.json')
-    .then(response => response.json())
-    .then(data => { 
-      Album = data;
-      videoIds = Object.keys(Album);
-      document.getElementById("myAlbum").innerHTML = videoIds.map(id => optAlbum(id)).join('\n')
-      const i = getRandomIntInclusive(0, videoIds.length - 1);
-      arg = {link: videoIds[i], albumObj: Album[videoIds[i]]};
-      outputHTML(arg);
-    });
