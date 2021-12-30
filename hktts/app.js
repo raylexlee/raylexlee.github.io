6784bafa38b1b9057d0b54d67ff1035987be3983
raylexlee <raylexym57lee@gmail.com>
2021-12-31 01:14:22 +0800

    Update entries.

---
+++ hktts/app.js
@@ -0,0 +1,39 @@
+var synth = window.speechSynthesis;
+var btnSpeak = document.getElementById('btnSpeak');
+var btnStop = document.getElementById('btnStop');
+var myVoice = document.getElementById('myVoice');
+var myContent = document.getElementById('myContent');
+var voices = [];
+var hkvoices, hkspeaker;
+
+function myInit() {
+ voices = synth.getVoices();
+ hkvoices = voices.filter(e => e.lang === 'zh-HK');
+ hkspeaker = hkvoices[0];
+ myVoice.innerText = hkspeaker.name;
+}
+
+myInit();
+
+
+function speak(){
+    if (synth.speaking) {
+        console.error('speechSynthesis.speaking');
+        return;
+    }
+    if (myContent.value !== '') {
+    var utterThis = new SpeechSynthesisUtterance(myContent.value);
+    utterThis.onend = function (event) {
+        console.log('SpeechSynthesisUtterance.onend');
+    }
+    utterThis.onerror = function (event) {
+        console.error('SpeechSynthesisUtterance.onerror');
+    }
+    utterThis.voice = hkspeaker;
+    utterThis.pitch = 1;
+    utterThis.rate = 1;
+    synth.speak(utterThis);
+  }
+}
+
+function stop() {}
