4bbc2658a620272069736ff8fa0c766a39589fa3
raylexlee <raylexym57lee@gmail.com>
2021-12-31 02:01:04 +0800

    Add error control

--- hktts/app.js
+++ hktts/app.js
@@ -9,8 +9,15 @@ var hkvoices, hkspeaker;
 function myInit() {
  voices = synth.getVoices();
  hkvoices = voices.filter(e => e.lang === 'zh-HK');
+ if (hkvoices.length === 0) {
+     btnSpeak.remove();
+     btnStop.remove();
+     myVoice.innerText = '唔好意思,  我唔識講廣東話';
+     return;
+ }
  hkspeaker = hkvoices[0];
  myVoice.innerText = hkspeaker.name;
+ btnStop.remove();
 }
 
 
