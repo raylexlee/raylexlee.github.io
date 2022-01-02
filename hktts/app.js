293d79f96a2202b80c62560b0c3d6d72147a5a45
raylexlee <raylexym57lee@gmail.com>
2022-01-02 23:54:21 +0800

    Update entries.

--- hktts/app.js
+++ hktts/app.js
@@ -1,10 +1,11 @@
-var synth = window.speechSynthesis;
-var btnSpeak = document.getElementById('btnSpeak');
-var btnStop = document.getElementById('btnStop');
-var myVoice = document.getElementById('myVoice');
-var myContent = document.getElementById('myContent');
-var voices = [];
-var hkvoices, hkspeaker;
+const synth = window.speechSynthesis;
+const btnSpeak = document.getElementById('btnSpeak');
+const btnStop = document.getElementById('btnStop');
+const myVoice = document.getElementById('myVoice');
+const myContent = document.getElementById('myContent');
+const rate = document.querySelector('#rate');
+let voices = [];
+let hkvoices, hkspeaker;
 
 function myInit() {
  voices = synth.getVoices();
@@ -12,14 +13,20 @@ function myInit() {
  if (hkvoices.length === 0) {
      btnSpeak.remove();
      btnStop.remove();
+     myContent.remove();
+     rate.remove();
      myVoice.innerText = '唔好意思,  我唔識講廣東話';
-     return;
  }
  hkspeaker = hkvoices[0];
- myVoice.innerText = hkspeaker.name;
- btnStop.remove();
+ console.log(hkspeaker.name);
+ myVoice.innerText = `${hkspeaker.localService ? '本機' : '雲端'}廣東話`;
+ btnStop.innerText = '暫停\繼續';
+ btnStop.onclick = pauseResume;
 }
 
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = myInit;
+}
 
 function speak(){
     if (synth.speaking) {
@@ -36,9 +43,18 @@ function speak(){
     }
     utterThis.voice = hkspeaker;
     utterThis.pitch = 1;
-    utterThis.rate = 1;
+    utterThis.rate = rate.value;
     synth.speak(utterThis);
   }
 }
 
-function stop() {}
+function pauseResume() {
+  if (synth.speaking) {
+    synth.pause();
+    return;
+  }
+  if (synth.paused) {
+    synth.resume();
+    return;
+  }    
+}
