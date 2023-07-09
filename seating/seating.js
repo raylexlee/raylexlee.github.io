2b9051e99ffd157a1cbd90da87f20fddd37a5424
Raylex Lee <raylexym57lee@gmail.com>
2023-07-09 11:58:41 +0800

    Add PRINT link

--- seating/seating.js
+++ seating/seating.js
@@ -2,24 +2,24 @@ let lastclick = undefined;
 function handleClick(element) {
   if (lastclick === element.alt) {
     lastclick = undefined;
-    status.innerText = statusDateTime;
+    status.innerHTML = statusDateTime;
     return;
   }
   if (lastclick === undefined) {
     lastclick = element.alt;
-    status.innerText = podcasts[images[lastclick]];
+    status.innerHTML = podcasts[images[lastclick]];
     return;
   }
   const a = lastclick;
   const b = element.alt;
   [images[a],images[b]] = [images[b], images[a]];
   lastclick = undefined;
-  status.innerText = statusDateTime;
+  status.innerHTML = statusDateTime;
   refresh();
 };
 const container = document.getElementsByClassName("grids-container")[0];
 const status = document.querySelector('p');
-const statusDateTime = status.innerText;
+const statusDateTime = status.innerHTML;
 const images = Object.keys(podcasts).sort(() => .5 - Math.random()).slice(0,30);
 const refresh = () => {
 const grids = images.map((p, i) => `<div><figure>
