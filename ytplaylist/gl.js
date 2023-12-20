f8d6ac46e68ce026e30fad0027fd565f45c81402
Raylex Lee <raylexym57lee@gmail.com>
2023-12-20 13:44:04 +0800

    Script used in devtools to extract pl

---
+++ ytplaylist/gl.js
@@ -0,0 +1,8 @@
+function raylex() {
+b=document.getElementsByTagName('a') 
+for (i=0; i<b.length; i++) {
+t=b[i].innerText; h=b[i].href;
+if (!t.match(/《/)) continue;
+s=h.indexOf('PL'); e=h.indexOf('&pp');
+c=t.replaceAll(' ','_')+' '+h.slice(s,e-1)
+console.log(c); } }
