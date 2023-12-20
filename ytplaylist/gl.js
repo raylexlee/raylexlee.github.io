9de29ce91bc972ba6e28ac4359a6d7ed4cfe801a
Raylex Lee <raylexym57lee@gmail.com>
2023-12-20 13:58:22 +0800

    Fix slice boundary error bug

--- ytplaylist/gl.js
+++ ytplaylist/gl.js
@@ -4,5 +4,5 @@ for (i=0; i<b.length; i++) {
 t=b[i].innerText; h=b[i].href;
 if (!t.match(/《/)) continue;
 s=h.indexOf('PL'); e=h.indexOf('&pp');
-c=t.replaceAll(' ','_')+' '+h.slice(s,e-1)
+c=t.replaceAll(' ','_')+' '+h.slice(s,e)
 console.log(c); } }
