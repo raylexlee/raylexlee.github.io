4a246b9e8d5169064ea67dfd82ffefbe3f2485dc
Raylex Lee <raylexym57lee@gmail.com>
2024-02-28 13:14:37 +0800

    Exclude leading 隨全

--- ytplaylist/gl.js
+++ ytplaylist/gl.js
@@ -4,7 +4,7 @@ for (i=0; i<b.length; i++) {
 t=b[i].innerText.replaceAll(' ','_'); h=b[i].href;
 if (t.startsWith('\n')) continue;
 if (/list=PL/.test(h) == false) continue;
-if (/^[1-9华現查已_]/.test(t)) continue;
+if (/^[1-9隨全华現查已_]/.test(t)) continue;
 s=h.indexOf('PL'); h=h.substr(s); e=h.indexOf('&');
 c=t+' '+h.slice(0,e);
 console.log(c); } }
