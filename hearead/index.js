3a8467fc809db64db90af77518893c5b2942c902
Raylex Lee <raylexym57lee@gmail.com>
2024-09-30 11:02:44 +0800

    Integrate qmttskoob and ibook

---
+++ hearead/index.js
@@ -0,0 +1,23 @@
+let type;
+const querystring = location.search;
+const params = (querystring != '') ? (new URL(document.location)).searchParams : 'none';
+if (params === 'none') window.location = 'index.html?type=qmttskoob.html';
+type =  params.get('type');
+type = type ? type : 'qmttskoob.html';
+document.body.onload = () => { 
+  insertRadioAtTopOfBody();
+  fetch(`triples.txt`)
+    .then(response => response.text())
+    .then(data => {
+      const AuthorBooks = data.replace(/\n+$/, "").split('\n');
+      const book = [];
+      AuthorBooks.forEach(AuthorBook => {
+        const [Lang, Author, Book] = AuthorBook.split(" ");
+        book.push([Lang, Book]);
+      });
+      const li_b = a => `<li>${a[0]} ${Language[a[0]].join(" ")} 
+<a href='qmttskoob.html?title=${a[1]}&lang=${a[0]}'>qmttskoob.html?title=${a[1]}&lang=${a[0]}</a></li>`;
+      document.querySelector('ul').innerHTML = book
+        .map(e =>   li_b(e)).join('\n');
+    });
+ };
---
+++ hearead/radiobutton.js
@@ -0,0 +1,56 @@
+const vtArray = [ 
+['qmttskoob.html', 'tts','HotPink'], 
+['ibook.html','txt', 'cyan']
+];
+const description = {
+'qmttskoob.html' : `For the best audio effect, please use the latest version of Microsoft Edge. Other browsers may work for mobile devices. Please refer to the Web Speech API at <a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API#speechsynthesis_2">SpeechSynthesis</a>。`,
+'ibook.html' : 'Please use the Read Aloud feature of the latest Microsoft Edge.'
+}
+function insertRadioAtTopOfBody() {
+  document.body.innerHTML = radioForm(vtArray) + document.body.innerHTML;
+  selectRadioButton(type);
+  document.querySelector('ul').style.backgroundColor = vtArray.filter( e => e.includes(type))[0][2];
+  document.querySelector('p').innerHTML = description[type];
+}
+function radioChange(radio) {
+    const selectedValue = radio.value;
+    const selectedText = radio.parentElement.textContent.trim();
+    const selectedBgcolor = window.getComputedStyle(radio.parentElement).backgroundColor;
+    
+//    console.log('Selected radio button value:', selectedValue);
+//    console.log('Selected radio button text:', selectedText);
+//    console.log(selectedBgcolor);
+    const links = document.querySelectorAll('a');
+    for (i=0; i < links.length; i++) {
+       links[i].href = links[i].href.replace(`${type}?title`,`${selectedValue}?title`);
+       links[i].innerText = links[i].innerText.replace(`${type}?title`,`${selectedValue}?title`);
+    } 
+    document.querySelector('ul').style.backgroundColor = selectedBgcolor;
+    document.querySelector('p').innerHTML = description[selectedValue];
+    type = selectedValue;
+    const base = window.location.href.split('?')[0];
+    const url = base.endsWith('group.html') ? `${base}?author=${document.title}&type=${type}` : `${base}?type=${type}` ;
+    qrcode.makeCode(url);
+}
+
+function selectRadioButton(value) {
+    const radioButton = document.querySelector(`input[name="fileType"][value="${value}"]`);
+    if (radioButton) {
+        radioButton.checked = true;
+    }
+}
+const radioOption = (v,t, c) => `
+            <label style="background-color: ${c}">
+                <input type="radio" name="fileType" value="${v}" onchange="radioChange(this)">${t}
+            </label>
+`;
+const radioForm = vtArr => `
+    <form>
+        <div class="radio-group">
+           ${vtArr.map(e => {
+             const [v, t, c] = e;
+             return radioOption(v,t, c);
+           }).join('\n')}
+        </div>
+    </form>
+`;
