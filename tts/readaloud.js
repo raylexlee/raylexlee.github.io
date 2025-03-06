ea19504c87fde9771fec69d8b3f64a821c19981b
Raylex Lee <raylexym57lee@gmail.com>
2025-03-06 13:44:30 +0800

    Enhance read aloud capability

---
+++ tts/readaloud.js
@@ -0,0 +1,128 @@
+let adjustment = 0.5;
+let crPosition=[];
+let nCharsRow, lineHeight; 
+let rowsLine=[]; 
+let numCharsLine=[];
+let punctuationPosition=[];
+let punctuationArray=[];
+let positionIndex = 0;
+const notAndroid = false;
+let utterThis = new SpeechSynthesisUtterance('Create utter this');
+ utterThis.onend = function (e) {
+   if (justCancel) {
+     justCancel = false;
+     return;
+   }
+   positionIndex++;
+   if (positionIndex === punctuationPosition.length) {
+     positionIndex = 0;
+   }
+   speak();
+ }
+ utterThis.onerror = function (event) {
+   console.error('SpeechSynthesisUtterance.onerror');
+ }
+// utterThis.onboundary = SyncAudioWithContent;
+  myContent.style.lineHeight=2;
+  myContent.onselect = e => {
+    if (programSelect >= 1) {
+       programSelect--;
+       return;
+    }
+    for (let i = 0; i < punctuationPosition.length; i++) {
+      if (punctuationPosition[i] >= myContent.selectionStart) {
+         positionIndex = i;
+         speak();
+         console.log('onselect ',i);
+         break;
+      }
+    }
+  }
+  myContent.onchange = gotoChapter;
+//  myRange.oninput = function() {
+//    const v = myRange.value;
+//    myContent.style.fontSize = `${20 + parseInt(v)}px`;
+//   CalculateScrollData(); // for rowsLine[], lineHeight, nCharsRow
+// };
+  document.body.onunload = function() {
+    if (synth.speaking) {
+      justCancel = true;
+      synth.cancel();
+    }
+  };
+function gotoChapter(PleaseSpeak = true) {
+       if (myContent.value == '') return;
+       currentVoice = nowVoice();
+       myContent.value = myContent.value.split('\n').filter(e => e.length >= 1).join('\n');
+       numCharsLine=myContent.value.split('\n').map(e => e.length);
+       rowsLine = Array(numCharsLine.length);
+       CalculateScrollData(); // for rowsLine[], lineHeight, nCharsRow
+       new ResizeObserver(CalculateScrollData).observe(myContent);
+       crPosition=[];
+       for (let valueIndex=0; valueIndex < myContent.value.length; valueIndex++) 
+         if (myContent.value[valueIndex] === '\n') {
+           crPosition.push(valueIndex);
+         }
+       punctuationArray = myContent.value.match(punctuationRegex);
+       punctuationPosition=[];
+       let punctuationIndex = 0;
+       for (let valueIndex=0; valueIndex < myContent.value.length; valueIndex++) 
+         if (myContent.value[valueIndex] === punctuationArray[punctuationIndex]) {
+           punctuationPosition.push(valueIndex);
+           punctuationIndex++;
+         }
+         if (synth.speaking) { 
+           justCancel = true;
+           synth.cancel();
+         }
+         if (PleaseSpeak) speak();
+}
+function speak(){
+    if (synth.speaking) {
+    //    console.error('speechSynthesis.speaking');
+        return;
+    }
+    if (myContent.value !== '') {
+    const start = (positionIndex >= 1) ? (punctuationPosition[positionIndex - 1] + 1) : 0;
+    const stop = punctuationPosition[positionIndex];
+    utterThis.voice = currentVoice;
+    utterThis.text = myContent.value.substring(start, stop);
+    utterThis.pitch = 1;
+    utterThis.rate = rate.value;
+    justCancel = true;
+    synth.cancel();
+    synth.speak(utterThis);
+    justCancel = false;
+    ScrollText(start);
+    programSelect = 1;
+    myContent.select();
+    programSelect = 2;
+    myContent.setSelectionRange(start, stop);
+  }
+}
+
+function ScrollText(charIndex)  {
+//  const fontSize = parseFloat(window.getComputedStyle(myContent).fontSize)
+//  const nCharsRow = Math.floor(myContent.clientWidth / fontSize)
+//  const lineHeight = myContent.scrollHeight / numCharsLine.map(e => Math.ceil(e / nCharsRow)).reduce((a,b) => a + b);
+  let lineIndex;
+  for (lineIndex=0; lineIndex < crPosition.length; lineIndex++)
+    if (charIndex <= crPosition[lineIndex]) break;
+  let topRow = 0;
+  if (lineIndex >=1)
+    topRow = rowsLine[lineIndex - 1];
+    // topRow = numCharsLine.slice(0, lineIndex).map(e => Math.ceil(e / nCharsRow)).reduce((a,b) => a + b);
+  const lastRow = Math.ceil(((lineIndex === 0) ? charIndex : (charIndex - crPosition[lineIndex-1])) / nCharsRow);
+  myContent.scrollTop = (lineHeight * (topRow + lastRow)) - (adjustment * myContent.clientHeight);
+}
+function CalculateScrollData() {
+  const fontSize = parseFloat(window.getComputedStyle(myContent).fontSize)
+  nCharsRow = Math.floor(myContent.clientWidth / fontSize)
+  rowsLine[0] = Math.ceil(numCharsLine[0] / nCharsRow);
+  let lineIndex = 1;
+  while (lineIndex < numCharsLine.length) {
+    rowsLine[lineIndex] = rowsLine[lineIndex - 1] + Math.ceil(numCharsLine[lineIndex] / nCharsRow);
+    lineIndex++;
+  }
+  lineHeight = myContent.scrollHeight / rowsLine[rowsLine.length - 1];
+}
--- tts/tts.js
+++ tts/tts.js
@@ -1,3 +1,6 @@
+const rtl = ['ar', 'he', 'ur', 'fa','ps'];
+let justCancel = false;
+let programSelect = 0;
 const synth = window.speechSynthesis;
 const btnSpeak = document.getElementById('btnSpeak');
 const btnStop = document.getElementById('btnStop');
@@ -6,11 +9,12 @@ const myContent = document.getElementById('myContent');
 const rate = document.querySelector('#rate');
 const narrators=[];
 let voices = [];
-let utterThis;
-
+let currentVoice;
+let punctuationRegex = /[；。！？;.!?]/gm;
+const googleRegex = /[；。！？，、,;.!?]/gm;
 function populateVoiceList() {
   voices = synth.getVoices().sort(function (a, b) {
-      const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
+      const aname = a.lang+a.name.toUpperCase(), bname = b.lang+b.name.toUpperCase();
       if ( aname < bname ) return -1;
       else if ( aname == bname ) return 0;
       else return +1;
@@ -30,19 +34,13 @@ function populateVoiceList() {
     voiceSelect.appendChild(option);
   }
   voiceSelect.selectedIndex = selectedIndex;
- btnStop.innerText = '暫停';
+ // currentVoice = nowVoice();
+ btnStop.innerText = 'STOP';
  btnStop.onclick = pauseResume;
- utterThis = new SpeechSynthesisUtterance('Create utter this');
- utterThis.onpause = function (event) {
-   console.log(event.charIndex);
-   console.log('SpeechSynthesisUtterance.onpause');
- }
- utterThis.onend = function (event) {
-   console.log('SpeechSynthesisUtterance.onend');
- }
- utterThis.onerror = function (event) {
-   console.error('SpeechSynthesisUtterance.onerror');
- }
+ btnSpeak.onclick = () => {
+   programSelect = 1;
+   speak();
+   }
 }
 
 populateVoiceList();
@@ -50,41 +48,33 @@ if (speechSynthesis.onvoiceschanged !== undefined) {
   speechSynthesis.onvoiceschanged = populateVoiceList;
 }
 
-function speak(){
-    if (synth.speaking) {
-        console.error('speechSynthesis.speaking');
-        return;
-    }
-    if (myContent.value !== '') {
-    utterThis.text = myContent.value;
+function nowVoice(){
+    let v;
     const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
     for(i = 0; i < voices.length ; i++) {
       if(voices[i].name === selectedOption) {
-        utterThis.voice = voices[i];
+        v = voices[i];
         break;
       }
     }
-    utterThis.pitch = 1;
-    utterThis.rate = rate.value;
-    synth.speak(utterThis);
-  }
+    const lang = v.voiceURI.replace(/\w+\s(\w+).*\s-\s(\w+)\s\((.*)\)/,'$2')
+    myContent.dir = (rtl.includes(lang)) ? 'rtl' : '';    
+      if (v.voiceURI.startsWith('Google')) punctuationRegex = googleRegex;
+    return v;
 }
 
-voiceSelect.onchange = function(){
+voiceSelect.onchange = function() {
+ currentVoice = nowVoice();
+  justCancel = true;
+  synth.cancel();
+  programSelect = 1;
   speak();
 }
 
 function pauseResume() {
-  if (synth.speaking !== true) {
-    return;
-  }
-  if (synth.paused) {
-    utterThis.rate = rate.value;
-    synth.resume();
-    return;
-  }
-  synth.pause();
-  if (synth.paused !== true) synth.cancel();
+    programSelect = 1;
+    justCancel = true;
+    synth.cancel();
 }
 
 function GetNarrators() {
@@ -99,6 +89,7 @@ function GetNarrators() {
     narrators.push(narrator) 
   }
   console.log(narrators);
-  myContent.value = narrators.map(e => `${e.name} ${e.local ? 'local' : 'remote'} ${e.langName} ${e.langCode} ${e.locName.replace(/ /g,'_')} ${e.locCode}`).join('\n');
+  myContent.value = narrators.map(e => `${e.name} ${e.local ? 'local' : 'remote'} ${e.langName} ${e.langCode} ${e.locName.replace(/ /g,'_')} ${e.locCode}.`).join('\n');
+  gotoChapter(false);
 }
 
