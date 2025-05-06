d26a48bc48009376540694336c03caa8d04fd2ec
Raylex Lee <raylexym57lee@gmail.com>
2025-05-06 17:47:29 +0800

    Add live radio stream

---
+++ radio/station.js
@@ -0,0 +1,6 @@
+const station = {
+"中山电台新锐967": "https://lhttp.qingting.fm/live/1277/64k.mp3",
+"中山电台快乐888": "https://lhttp.qingting.fm/live/1278/64k.mp3",
+"香港電台第一台": "https://stm.rthk.hk/radio1",
+"Apple-FM": "https://ice.apple-fm.net:8443/apple-fm.mp3",
+}
