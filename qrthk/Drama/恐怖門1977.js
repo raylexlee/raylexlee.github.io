db366a19e250a104bd217e82741c6c22128dbedf
Raylex Lee <raylexym57lee@gmail.com>
2025-07-19 01:46:20 +0800

    New radio drama added

---
+++ qrthk/Drama/恐怖門1977.js
@@ -0,0 +1,9 @@
+ var radiodrama = {
+program_infix: "3terror/ghoststory_door",
+episodes: 3,
+digit: 0
+}
+const p = `改編自余過的小說。某國的一名將軍，視殺人為嗜好和藝術，以不同的酷刑來折磨敵對黨，對付逆他意的人，其中以恐怖門最殘酷，任何人進入恐怖門，就會被折磨到求生不得，求死不能．．．
+`
+const pre = `編導: 李安求 播出年份: 1977
+`
