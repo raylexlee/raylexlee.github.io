1ae97437c97226f44927143fd3853160230e7b67
Raylex Lee <raylexym57lee@gmail.com>
2025-07-19 20:27:59 +0800

    New radiodrama

---
+++ qrthk/Drama/緣訂三生.js
@@ -0,0 +1,10 @@
+ var radiodrama = {
+program_infix: "9solo/rd_threelives_",
+episodes: 2,
+digit: 0
+}
+const p = `沈燦若是嘉興有名的才子，與妻子王氏恩愛非常，如膠似漆。一年，沈燦若赴考途中，深夜夢得一道長指點，指他必在喪妻之後方有功名。驚醒的沈燦若心亂如麻……
+`
+const pre = `演員表
+講述：鍾偉明
+`
