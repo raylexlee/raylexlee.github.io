dfe2e82f75798573fe543046507b57a365db23f9
Raylex Lee <raylexym57lee@gmail.com>
2025-08-11 17:32:39 +0800

    Add mobile screen off support

--- entts/enttskoob.js
+++ entts/enttskoob.js
@@ -1,4 +1,5 @@
 let adjustment = 0.6;
+let audio;
 const googleLimit = 57;
 let programSelect = 0;
 let title, myContent, myChapterList, myRange, myBook, myAutoplay;
@@ -86,6 +87,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 const contentUrl = chapter => `text/${title}/${chapter.substring(0,nDigits)}.txt`;
 function myInit() {
   document.title = title;
+  audio = document.getElementById('audio');
+  audio.onended = () => { audio.play(); };
+  audio.onplay = speak;
+  audio.onpause = pauseResume;
   myContent = document.getElementById('myContent');
   myContent.style.lineHeight=2;
   myChapterList = document.getElementById('myChapterList');
@@ -269,6 +274,7 @@ function speak(){
     justCancel = true;
     synth.cancel();
     synth.speak(utterThis);
+    audio.play();
     justCancel = false;
 //    const portion = start / myContent.value.length;
 //    myContent.scrollTop = portion * myContent.scrollHeight - adjustment * myContent.offsetHeight;
@@ -291,6 +297,7 @@ function pauseResume() {
   if (synth.speaking !== true) {
     return;
   }
+  audio.pause();
   synth.cancel();
   localStorage.setItem('wspa_positionIndex'+title, positionIndex);
   updateQR(title, activeEpisode, positionIndex);
