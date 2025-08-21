08917246ff14dfdc6c7741a18b55f18c49fae84d
Raylex Lee <raylexym57lee@gmail.com>
2025-08-21 20:14:27 +0800

    Add 飄　第二十三章

--- edgetts/makeEdgeTTS.sh
+++ edgetts/makeEdgeTTS.sh
@@ -6,10 +6,11 @@ VTT=$(sed 's/txt$/vtt/' <<< $TXT)
 MP3=$(sed 's/txt$/mp3/' <<< $TXT)
 SVG=$(sed 's/txt$/svg/' <<< $TXT)
 start=$(date +%s)
-edge-tts --voice zh-HK-WanLungNeural --rate=-15% --file $TXT --write-media $MP3 --write-subtitles $SRT
+edge-tts --voice zh-HK-WanLungNeural --file $TXT --write-media $MP3 --write-subtitles $SRT
 echo WEBVTT > $VTT
 sed 's/,/./g' < $SRT >> $VTT
 sed 's/RaylexLee/'"$TITLE"'/' < RaylexLee.svg > $SVG
 echo $(sed 's#.*/\([^/]*\)\.txt$#\1#' <<< $TXT) $TITLE >> pair.txt
+rm $TXT; rm $SRT
 end=$(date +%s)
-echo ==== $((end = start))s $MP3
+echo ==== $((end - start))s $MP3
