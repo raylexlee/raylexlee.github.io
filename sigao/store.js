da38f235d628c34e083bf4df8e18a6d64cc43798
Raylex Lee <raylexym57lee@gmail.com>
2025-09-18 20:37:58 +0800

    Use new layout

---
+++ sigao/store.js
@@ -0,0 +1,57 @@
+let myGroup, myBook;
+const book = {};
+const lastBookInGroupStored = g => `lastSigaoBookInGroup${g}`
+const lastBookStored = `lastSigaoBook`
+let lastBook;
+let lastGroup;
+const optionGroup = g => `<option value="${g}" ${(g == lastGroup) ? 'selected' : ''}>${g}</option>`;
+const optionBook = b => `<option value="${b}" ${(b == lastBook) ? 'selected' : ''}>${b}</option>`;
+function isEdgeAndroid() {
+  const userAgent = navigator.userAgent.toLowerCase();
+  return userAgent.includes('edg') && userAgent.includes('android');
+}
+async function fetchText(file) {
+  const response = await fetch(file);
+  const text = await response.text();
+  return text;
+}
+document.addEventListener("DOMContentLoaded", function(event) { myInit(); });
+async function myInit() { 
+const  myFootlineSetting = document.getElementById('myFootlineSetting');
+const  myFootline = document.getElementById('myFootline');
+  if (isEdgeAndroid()) {
+    myFootline.style.minHeight = '70px';
+    myFootlineSetting.style.minHeight = '70px';    
+  } else {
+    myFootline.style.display = 'none';
+  }
+  const data = await fetchText(`pairs.txt`);
+  const AuthorBooks = data.replace(/\n+$/, "").split('\n');
+  AuthorBooks.forEach(AuthorBook => {
+    const [Author, Book] = AuthorBook.split(" ");
+    if (Author in book) {
+      book[Author].push(Book);
+    } else {  
+        book[Author] = [ Book ];
+     }
+  });
+  myGroup = document.getElementById('myGroup');
+  myBook = document.getElementById('myBook');
+  lastBook = localStorage.getItem(lastBookStored);
+  lastBook = lastBook ? lastBook : '瑪竇福音-Matthew';
+  lastGroup = Object.keys(book).filter(g => book[g].includes(lastBook))[0];
+  myGroup.innerHTML = Object.keys(book).map(g => optionGroup(g)).join('\n');
+  myBook.innerHTML = book[lastGroup].map(b => optionBook(b)).join('\n');
+  myGroup.onchange = () => {
+    lastBook = localStorage.getItem(lastBookInGroupStored(myGroup.value));
+    lastBook = lastBook ? lastBook : book[myGroup.value][0];
+    myBook.innerHTML = book[myGroup.value].map(b => optionBook(b)).join('\n');
+  }
+}
+const ReadAloud = () => { 
+  lastBook = myBook.value;
+  lastGroup = myGroup.value;
+  localStorage.setItem(lastBookStored, lastBook);
+  localStorage.setItem(lastBookInGroupStored(lastGroup), lastBook);
+  window.location = `zhttsaloud.html?title=${myBook.value}`; 
+}
