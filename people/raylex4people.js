f9b33e261bd67635a9b98dd1df35f6dc499e7642
Raylex Lee <raylexym57lee@gmail.com>
2026-04-03 00:50:12 +0800

    古今風雲人物 return...

--- people/raylex4people.js
+++ people/raylex4people.js
@@ -7,5 +7,5 @@ h=t.href;
 x=t.innerText.split('\n');
 if (!h.startsWith(s)) continue;
 d=x[0].split('/');
-c=d[2]+'-'+d[1]+'-'+d[0]+' '+h.substring(s.length)+' '+x[2];
+c=d[2]+d[1]+d[0]+' '+h.substring(s.length)+' '+x[2];
 console.log(c); } }
