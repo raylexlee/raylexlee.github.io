cc277665e7d8cd7b062e7b38e447dc598d5627c5
Raylex Lee <raylexym57lee@gmail.com>
2026-04-28 19:21:49 +0800

    Also make lastDate.py

--- rthkPlaylist/create_last_date_tex.sh
+++ rthkPlaylist/create_last_date_tex.sh
@@ -1,1 +1,8 @@
-for i in $(ls *m3u8); do j=$(sed 's#\(.*\).....$#\1#' <<< $i); tail -2 $i | head -1 | sed 's#.*\(....\)\(..\)\(..\).$#\1\2\3 \3/\2/\1 '"$j"'#'; done | sort > last_date.txt
+echo last_date = { > lastDate.py
+for i in $(ls *m3u8) 
+do 
+  j=$(sed 's#\(.*\).....$#\1#' <<< $i) 
+  tail -2 $i | head -1 | sed 's#.*\(....\)\(..\)\(..\).$#\1\2\3 \3/\2/\1 '"$j"'#' 
+  tail -2 $i | head -1 | sed 's#.*\(....\)\(..\)\(..\).$#='"$j"'=: =\3/\2/\1=,#' | sed 's#=#"#g'>> lastDate.py 
+done | sort > last_date.txt
+echo }>> lastDate.py
