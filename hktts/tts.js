1c10baac19b77ac38724356126fe5c3a6f0586f6
Raylex Lee <raylexlee@outlook.com>
2026-08-01 11:52:36 +0800

    Take all en voices

--- hktts/tts.js
+++ hktts/tts.js
@@ -12,7 +12,8 @@ const nameSpeaker = name => {
 };
 
 function populateVoiceList() {
-  voices = synth.getVoices().filter(v => v.lang.startsWith('en') && 'AU_US_GB_CA_IE_NZ_HK'.includes(v.lang.slice(3,5)));
+  // voices = synth.getVoices().filter(v => v.lang.startsWith('en') && 'AU_US_GB_CA_IE_NZ_HK'.includes(v.lang.slice(3,5)));
+  voices = synth.getVoices().filter(v => v.lang.startsWith('en')).sort((a,b) => a.lang.localeCompare(b.lang));
   var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
   voiceSelect.innerHTML = '';
   for(i = 0; i < voices.length ; i++) {
