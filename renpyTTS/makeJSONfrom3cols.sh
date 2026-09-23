74b6bae7ab3b123b8625b647cfb093f74b75f02c
Raylex Lee <raylexlee@outlook.com>
2026-09-24 00:52:01 +0800

    Update voice matching

--- renpyTTS/makeJSONfrom3cols.sh
+++ renpyTTS/makeJSONfrom3cols.sh
@@ -2,3 +2,6 @@
 GAME=${1?'missing game name eg ReclaimingtheLost..'}
 grep ^[a-z] "$GAME"_3col.txt | awk '{print $1,$3;}' > "$GAME".txt
 ./genJSON.sh "$GAME"
+git add -A
+git commit -m 'Update voice matching'
+git push origin master
